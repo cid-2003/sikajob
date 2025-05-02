3dcab4c24b8a7427cef67d14719cb6d4269b048c
Cédrick <cedrickemmanuel69@gmail.com>
2025-05-02 16:00:36 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -34,7 +34,7 @@ return [
     */
 
     'mailers' => [
-        'smtp' => [
+        /*'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
@@ -83,7 +83,7 @@ return [
                 'smtp',
                 'log',
             ],
-        ],
+        ],*/
     ],
 
     /*

8d3f8816a70582e6f57b8d35522a84582160cb69
Cédrick <cedrickemmanuel69@gmail.com>
2025-05-02 14:45:12 +0100

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    #'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'null'),
 
     /*
     |--------------------------------------------------------------------------
@@ -28,9 +28,9 @@ return [
     |
     */
 
-    /*'connections' => [
+    'connections' => [
 
-        'pusher' => [
+        /*'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
@@ -55,7 +55,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-        ],
+        ],*/
 
         'log' => [
             'driver' => 'log',
@@ -65,6 +65,6 @@ return [
             'driver' => 'null',
         ],
 
-    ],*/
+    ],
 
 ];

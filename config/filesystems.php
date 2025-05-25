85c82e0a2bd7f12741fa24994c023e7a599a26fb
Cédrick <cedrickemmanuel69@gmail.com>
2025-05-25 15:12:36 +0100

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,7 +44,7 @@ return [
             'throw' => false,
         ],
 
-        /*'s3' => [
+        's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
@@ -54,7 +54,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
-        ],*/
+        ],
 
     ],
 
